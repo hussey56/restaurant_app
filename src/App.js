2b836e6f46f37a406a66d313db7c429016c5409d
hussey56 <hassankhan032370@gmail.com>
2023-12-02 00:54:47 +0500

    nav work

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React from 'react'
 import {useValue} from './context/ContextProvider'
+import Navbar from './component/Navbar/Navbar';
 const App = () => {
   const {
     state: { login },
     dispatch,
   } = useValue();
-  const handleClose =()=>{
-    dispatch({
-      type: "OPEN_LOGIN",
-    });
-  }
-  const handleOpen =()=>{
-    dispatch({
-      type: "ClOSE_LOGIN",
-    });
-  }
+ 
 
   return (
-    <div>
-    {login ? 'Login Open' : "Login Close"}
-
-      <button onClick={login ? handleClose : handleOpen}>{login ? 'Close' :'Open'}</button>
-    </div>
+   
+    <Navbar/>
   )
 }
 
